83ffb11e256116283e72117b43701e56fb11f37f
jcbellido <jcbellido@gmail.com>
2016-03-20 14:18:38 +0100

    Initial implementation of graph object ... super simple stuff
    Signed-off-by: jcbellido <jcbellido@gmail.com>

--- Homework_coursera/DijstraShortestPathImplementation/DijstraShortestPathImplementation.cpp
+++ Homework_coursera/DijstraShortestPathImplementation/DijstraShortestPathImplementation.cpp
@@ -1,12 +1,47 @@
 // DijstraShortestPathImplementation.cpp : Defines the entry point for the console application.
 //
+// Create a graph as an abstract data type: using C++ classes
+//		assume UNDIRECTED graphs
+//		edge lists
+//		connection matrixes
+//		random graph constructor
+//			edge density (0..1)
+//			edge distance range as a parameter
+// Dijstra's algorithm should be a class member function 
 
 #include <iostream>
+#include <vector>
+#include <cassert>
 
 using namespace std;
+
+template<class weight>
+class Graph
+{
+private:
+	// let's use matrixes for now ... makes thing simpler ...
+	vector<vector<weight>> m_matrix_weight;
+public: 
+	Graph(unsigned int vertex_count, float edge_density, weight min_weight, weight max_weight)
+	{
+		// static_assert(0.0f < edge_density <= 1.0f, "Edge should be a value between (0, 1]");
+		// static_assert(max_weight > min_weight, "Weight should be a valid range, max > min");
+		// static_assert(vertex_count >= 2, "Vertex count should be 2 or greater");
+		// move 
+
+
+	}
+
+	~Graph()
+	{
+		std::cout << "Graph deletion ... " << std::endl;
+	}
+};
 
 int main()
 {
 	cout << "Executing Dijstra shortest" << endl;
+	auto g = new Graph<float>(50, 0.1f, 1.0f, 10.0f);
+
 	return 0;
 }
