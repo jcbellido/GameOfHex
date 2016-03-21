9d71bae42b7b61b6ddf1bd0e6b040eeaca56d811
jcbellido <jcbellido@gmail.com>
2016-03-21 20:15:00 +0100

    fixed initialization, done the first two functions of graph traversal

--- Homework_coursera/DijstraShortestPathImplementation/DijstraShortestPathImplementation.cpp
+++ Homework_coursera/DijstraShortestPathImplementation/DijstraShortestPathImplementation.cpp
@@ -12,6 +12,7 @@
 #include <ctime>
 #include <exception>
 #include <iostream>
+#include <iomanip>
 #include <vector>
 
 using namespace std;
@@ -55,13 +56,19 @@ public:
 			delete(m_matrix_weight);
 	}
 
+	// Convenience debug function
 	void print()
 	{
-		for (auto it = m_matrix_weight->begin(); it != m_matrix_weight->end(); it++)
+		cout << "Edge density: " << edge_density << endl;
+		cout << "Vertex count: " << vertex_count << endl; 
+		for (auto row : *m_matrix_weight)
 		{
-			for (vector<weight>::iterator inner_it = *it->begin(); inner_it != *it->begin(); inner_it++)
+			for (auto column : row)
 			{
-				cout << (*inner_it) << " ";
+				if (column == 0)
+					cout << "0.0 ";
+				else
+					cout << setprecision(2) << column << " ";
 			}
 			cout << endl;
 		}
@@ -76,7 +83,7 @@ private:
 
 	inline bool does_edge_exists()
 	{
-		return this->edge_density < rand_zero_to_one();
+		return rand_zero_to_one() < this->edge_density;
 	}
 
 	inline weight get_random_weight()
@@ -90,29 +97,45 @@ private:
 protected: 
 	virtual void construct_graph_representation(weight min_weight, weight max_weight)
 	{
-		m_matrix_weight = new vector<vector<weight>>(vertex_count);
+		m_matrix_weight = new vector<vector<weight>>();
 	
-		for (unsigned int i = 1; i < vertex_count; ++i)
+		for (unsigned int i = 1; i <= vertex_count; ++i)
 			m_matrix_weight->push_back(vector<weight>(i));	// Initialize the structure
 
 		// Traverse the lower half of the structure and then mirror it
 		auto row_iterator = m_matrix_weight->begin();
 		for (unsigned int i = 0; i < vertex_count; ++i)
 		{
-			vector<weight> row = *row_iterator;
-			for (unsigned int j = 0; j < i; ++j)
+			for (auto& column_iterator : *row_iterator)
 			{
 				if (does_edge_exists())
 				{
-					row.push_back(get_random_weight());
+					column_iterator = get_random_weight();
 				}
 				else
 				{
-					row.push_back(0);
+					column_iterator = 0;
 				}
 			}
 			row_iterator++;
 		}
+	}
+
+	weight get_vertex_weight(int origin, int destination)
+	{
+		if (destination > origin)
+			swap(origin, destination);	// Note: This implementation keeps the lower part of the matrix
+
+		return *(m_matrix_weight[origin][destination]);
+	}
+	
+	bool is_connected(int origin, int destination)
+	{
+		if (destination > origin)
+			swap(origin, destination);	// Note: This implementation keeps the lower part of the matrix
+		
+		if (*(m_matrix_weight[origin][destination]) > 0)
+			return true;
 	}
 };
 
@@ -120,7 +143,7 @@ int main()
 {
 	srand(time(0));
 	cout << "Executing Dijstra shortest" << endl;
-	auto g = new Graph<float>(10, 0.1f, 1.0f, 10.0f);
+	auto g = new Graph<float>(10, 0.5f, 1.0f, 10.0f);
 	g->print();
 	delete(g);
 	return 0;
