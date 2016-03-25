536100d4c2de449eb7ece98c2fb738ad4fabbd08
jcbellido <jcbellido@gmail.com>
2016-03-25 13:44:58 +0100

    Calculate paths for graphs

--- Homework_coursera/DijstraShortestPathImplementation/DijstraShortestPathImplementation.cpp
+++ Homework_coursera/DijstraShortestPathImplementation/DijstraShortestPathImplementation.cpp
@@ -1,13 +1,16 @@
-﻿// DijstraShortestPathImplementation.cpp : Defines the entry point for the console application.
-//
-// Create a graph as an abstract data type: using C++ classes
-//		assume UNDIRECTED graphs
-//		edge lists
-//		connection matrixes
-//		random graph constructor
-//			edge density (0..1)
-//			edge distance range as a parameter
-// Dijstra's algorithm should be a class member function 
+﻿// DijstraShortestPathImplementation.cpp 
+// Some notes about the graph implementation: 
+// 1. I am using a matrix approach to keep the graph structure
+// 2. The grah is non directed
+// 3. Edges that go from a node into itself are not allowed
+// 4. For practice purposes I'm keeping the edge cost as a templated class
+
+// About the algorithm itself
+// I'm using sets as data containers
+// All the Dijkstra 
+
+// General implementation notes: 
+// 1. I was curious about the use of exceptions in C++ and I'm using them to report bad parameters
 
 #include <ctime>
 #include <exception>
@@ -32,10 +35,8 @@ inline int rand_int_in_positive_range(unsigned int min, unsigned int max)
 	return min + (rand() % (int)(max - min + 1));
 }
 
-
 // ----------------------------------------------------------------------------
-// Graph clases
-
+// Graph representation
 template<class weight>
 class Graph
 {
@@ -43,7 +44,7 @@ public:
 	Graph(unsigned int vertex_count, float edge_density, weight min_weight, weight max_weight)
 	{
 		if (vertex_count == 0)
-			throw invalid_argument("Vertex count must be bigger than zero");
+			throw invalid_argument("Vertex count must be greater than zero");
 
 		if ((0.0f >= edge_density) || (edge_density > 1.0f))
 			throw invalid_argument("Edge density must be in the range (0, 1]");
@@ -56,66 +57,33 @@ public:
 		this->min_weight = min_weight;
 		this->vertex_count = vertex_count;
 
-		construct_graph_representation(min_weight, max_weight);
+		constructGraphRepresentation(min_weight, max_weight);
 	}
 
-	~Graph()
-	{
-		if (m_matrix_weight != nullptr)
-			delete(m_matrix_weight);
-	}
-
-	// Convenience debug function
-	void print()
-	{
-		cout << "Edge density: " << edge_density << endl;
-		cout << "Vertex count: " << vertex_count << endl;
-		for (auto row : *m_matrix_weight)
-		{
-			for (auto column : row)
-			{
-				if (column == 0)
-					cout << "0.0 ";
-				else
-					cout << setprecision(2) << column << " ";
-			}
-			cout << endl;
-		}
-	}
-
-	bool DoesVertexExists(unsigned int key) const
-	{
-		return  key < vertex_count;
-	}
-
-	unsigned int VertexCount() const
-	{
-		return vertex_count;
-	}
+	template<class weight>
+	friend ostream& operator<<(ostream& cout, const Graph<weight>& g);
 
 private:
-	vector<vector<weight>>* m_matrix_weight = nullptr;
+	vector<vector<weight>> m_matrix_weight;
 	float edge_density = 0.0f; 
 	unsigned int vertex_count = 0; 
 	weight min_weight;
 	weight max_weight;
 
-	virtual void construct_graph_representation(weight min_weight, weight max_weight)
+	virtual void constructGraphRepresentation(weight min_weight, weight max_weight)
 	{
-		m_matrix_weight = new vector<vector<weight>>();
-
 		for (unsigned int i = 1; i <= vertex_count; ++i)
-			m_matrix_weight->push_back(vector<weight>(i));	// Initialize the structure
+			m_matrix_weight.push_back(vector<weight>(i));	// Initialize the structure
 
 		// Traverse the lower half of the structure and then mirror it
 		// Also: avoids edges that starts at ends on the same node
-		auto row_iterator = m_matrix_weight->begin();
+		auto row_iterator = m_matrix_weight.begin();
 		for (unsigned int i = 0; i < vertex_count; ++i)
 		{
 			unsigned int j = 0;
 			for (auto& column_iterator : *row_iterator)
 			{
-				if ((i != j) && does_edge_exists())
+				if ((i != j) && doesEdgeExists())
 				{
 					column_iterator = get_random_weight();
 				}
@@ -129,7 +97,7 @@ private:
 		}
 	}
 
-	inline bool does_edge_exists()
+	inline bool doesEdgeExists()
 	{
 		return rand_zero_to_one() < this->edge_density;
 	}
@@ -143,16 +111,27 @@ private:
 	}
 
 public: 
-	weight get_edge_value(unsigned int origin, unsigned int destination) const
+
+	bool DoesVertexExists(unsigned int key) const
+	{
+		return  key < vertex_count;
+	}
+
+	unsigned int VertexCount() const
+	{
+		return vertex_count;
+	}
+
+	weight GetEdgeValue(unsigned int origin, unsigned int destination) const
 	{
 		if (destination > origin)
 			swap(origin, destination);	// Note: This implementation keeps the lower part of the matrix
 
-		vector<weight> row = (*m_matrix_weight)[origin];
+		vector<weight> row = m_matrix_weight[origin];
 		return row[destination];
 	}
 	
-	bool adjacent(unsigned int origin, unsigned int destination) const
+	bool IsAdjacent(unsigned int origin, unsigned int destination) const
 	{
 		if (destination == origin)
 			return true;
@@ -160,25 +139,69 @@ public:
 		if (destination > origin)
 			swap(origin, destination);	// Note: This implementation keeps the lower part of the matrix
 		
-		if (get_edge_value(origin, destination) > 0)
+		if (GetEdgeValue(origin, destination) > 0)
 			return true;
 		
 		return false;
 	}
 
-	const vector<unsigned int> neighbors(unsigned int origin) const
+	const vector<unsigned int> Neighbors(unsigned int origin) const
 	{
 		vector<unsigned int> output;
 		for (unsigned int i = 0; i < vertex_count; i++)
 		{
 			if (origin != i)
-				if (adjacent(origin, i))
+				if (IsAdjacent(origin, i))
 					output.push_back(i);
 		}
 		return output;
 	}
+
+	weight PathWeight(stack<unsigned int> path) const
+	{
+		if (path.size() <= 1)
+			return 0;
+
+		weight output = 0;
+		unsigned int origin = path.top(); 
+		path.pop();
+		unsigned int destination;
+		do
+		{
+			destination = path.top();
+			path.pop();
+			if (!IsAdjacent(origin, destination))
+				throw invalid_argument("Invalid edge on path");
+
+			output += GetEdgeValue(origin, destination);
+			swap(origin, destination);
+		} while (!path.empty());
+		return output; 
+	}
 };
 
+template<class weight>
+ostream& operator<<(ostream& cout, const Graph<weight>& g)
+{
+	cout << "Edge density: " << g.edge_density << endl;
+	cout << "Vertex count: " << g.vertex_count << endl;
+	for (auto row : g.m_matrix_weight)
+	{
+		for (auto column : row)
+		{
+			if (column == 0)
+				cout << "0.0 ";
+			else
+				cout << setprecision(2) << column << " ";
+		}
+		cout << endl;
+	}
+	return cout;
+}
+
+
+// ----------------------------------------------------------------------------
+// Dijstra shortest path algorithm implementation 
 template<class weight>
 class NodeDistance
 {
@@ -190,13 +213,10 @@ public:
 	NodeDistance() : nodeIndex(-1), pathAggregateCost(-1) { }
 	NodeDistance(unsigned int nodeIndex, weight currentWeight) : nodeIndex(nodeIndex), pathAggregateCost(currentWeight) { }
 
-// properties
-public: 
 	const weight getCost() const { return pathAggregateCost; }
 	void setCost(weight newCost) { pathAggregateCost = newCost; }
 	const unsigned int getNodeIndex() const { return nodeIndex; }
 
-public:
 	bool operator== (const NodeDistance<weight>& other) const
 	{
 		return this->getNodeIndex() == other.getNodeIndex();
@@ -211,6 +231,8 @@ public:
 	}
 };
 
+// Dijstra Algorithm 
+// Uses the variable names from the wikipedia pseudocode implementation
 template<class weight>
 class DijkstraAlgorithm
 {
@@ -220,14 +242,15 @@ private:
 	vector<weight> m_dist;
 	vector<int> m_prev;
 
-	const weight WINFINITY = numeric_limits<weight>::max();
+	const weight WINFINITY = numeric_limits<weight>::max();	// weight-infinity to avoid constant clash
 	const int UNDEFINED = numeric_limits<int>::min();
 
 public:
 	DijkstraAlgorithm(const Graph<weight> &graph) : m_graph(graph)  { }
 
 	/// Returns the shortest path for the specific algorithm
-	stack<unsigned int> SolveAlgorithm(unsigned int source, unsigned int destination)
+	/// or an empty stack if no path was found
+	stack<unsigned int> GetPath(unsigned int source, unsigned int destination)
 	{
 		if (!m_graph.DoesVertexExists(source) || !m_graph.DoesVertexExists(destination))
 			throw invalid_argument("Sources or Destinations indexes not present in the graph");
@@ -239,8 +262,6 @@ public:
 			return trivialPath;
 		}
 
-		cout << "Looking for a path between: " << source <<  " and " << destination << endl;
-
 		initialization(source);	
 		while (!m_q.empty())
 		{
@@ -249,10 +270,10 @@ public:
 			if (u.getNodeIndex() == destination)
 				break;
 
-			auto neighbors = m_graph.neighbors(u.getNodeIndex());
+			auto neighbors = m_graph.Neighbors(u.getNodeIndex());
 			for (auto v : neighbors)
 			{
-				weight alt = m_dist[u.getNodeIndex()] + m_graph.get_edge_value(u.getNodeIndex(), v);
+				weight alt = m_dist[u.getNodeIndex()] + m_graph.GetEdgeValue(u.getNodeIndex(), v);
 				if (alt < m_dist[v])
 				{
 					auto f = find(m_q.begin(), m_q.end(), NodeDistance<weight>(v, m_dist[v]));
@@ -279,10 +300,11 @@ public:
 		}
 		else
 		{
-			return stack<unsigned int>(); 
+			return stack<unsigned int>();	// Empty stack : no path found
 		}
 	}
-	
+
+private: 	
 	void clear()
 	{
 		m_q.clear();
@@ -308,6 +330,8 @@ public:
 	}
 };
 
+// ----------------------------------------------------------------------------
+
 int main()
 {
 	// srand(time(0));
@@ -315,10 +339,11 @@ int main()
 	srand(0);
 	auto graph = new Graph<float>(5, 0.1f, 1.0f, 10.0f);
 	auto algorithm = new DijkstraAlgorithm<float>(*graph);
-	graph->print();
-	auto path = algorithm->SolveAlgorithm(3, 4);
+	cout << "Graph printing: " << (*graph) << endl;
+	auto path = algorithm->GetPath(0, 2);
 	if (!path.empty())
 	{
+		cout << "Cost: " << setprecision(2) << graph->PathWeight(path) << " path: "; 
 		while (!path.empty())
 		{
 			cout << path.top() << " ";
@@ -327,7 +352,7 @@ int main()
 	}
 	else
 	{
-		cout << "no path found" << endl; 
+		cout << "No path found" << endl; 
 	}
 	cout << endl;
 	delete(algorithm);
