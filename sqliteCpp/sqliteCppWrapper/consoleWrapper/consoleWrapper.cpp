0f61802652ae9e531bb452ddf2a64c4523896804
jcbellido <jcbellido@gmail.com>
2017-02-21 20:44:48 +0100

    First running console

--- sqliteCpp/sqliteCppWrapper/consoleWrapper/consoleWrapper.cpp
+++ sqliteCpp/sqliteCppWrapper/consoleWrapper/consoleWrapper.cpp
@@ -1,11 +1,23 @@
-// consoleWrapper.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
-
+#include "sqlite3\sqlite3.h"
+#include <stdio.h>
 
 int main()
 {
+	sqlite3 * connection = nullptr;
+
+	int result = sqlite3_open(":memory:", &connection);
+
+	if (SQLITE_OK != result)
+	{
+		printf("%s/n", sqlite3_errmsg(connection));
+		sqlite3_close(connection);
+		return result;
+	} 
+	else
+	{ 
+		sqlite3_close(connection);
+	}
+	
     return 0;
 }
 
